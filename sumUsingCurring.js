4db5f614aca0874cc6d11cc5dfd90c3744fd8f22
Akash Karwande <akash.karwande@oracle.com>
2025-02-09 10:55:59 +0530

    sum using function curring

---
+++ sumUsingCurring.js
@@ -0,0 +1,12 @@
+// sum()(2)(4)...() --> 6
+
+function sum(a) {
+    return function(b) {
+        if(b) return sum(a+b)
+            return a
+    }
+}
+
+let res = sum(3)(2)(4)();
+
+console.log(res)
