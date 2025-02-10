c4517b3bb6fb90867ce407e79e46bdb4ebc6bd6c
Akash Karwande <akash.karwande@oracle.com>
2025-02-10 10:59:58 +0530

    array chunk

---
+++ arrChunk.js
@@ -0,0 +1,12 @@
+// arrChunk([1,2,3], 1) --> [[1],[2],[3]]
+
+
+function arrChunk (arr, size) {
+    let result = []
+
+    for (let i = 0; i < arr.length; i= i + size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result
+}
+console.log(arrChunk([1,2,3],2))
